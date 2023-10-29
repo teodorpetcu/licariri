d6ea1b088b8d7db2bce154f5edd9d086fb5ba324
Teodor Petcu <petcuteodor03@gmail.com>
2023-10-29 12:54:57 +0200

    Add a database of articles (sqlite3)

--- server.js
+++ server.js
@@ -1,16 +1,68 @@
 const express = require("express");
-const path = require("path");
+const marked = require("marked");
+const sqlite3 = require("sqlite3");
+const fs = require("fs");
 
 const app = express();
-const webaddress = "http://localhost";
 const port = 8000;
 
-app.use(express.static(__dirname + '/public'))
+const db_path = __dirname + "/articles/articles.sql"
 
-app.get('/', (_, res) => {
-    res.sendFile(path.join(__dirname, "/pages/template-article.html"))
+const db = new sqlite3.Database(db_path, (err) => {
+    if (err) {
+        console.error(err);
+    }
+})
+
+const init_db = () => {
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS articles
+        (
+            id          TEXT NOT NULL,
+            title       TEXT NOT NULL,
+            date        TEXT,
+            UNIQUE (id)
+        );
+        CREATE TABLE IF NOT EXISTS article_tags
+        (
+            id      TEXT NOT NULL,
+            tag     TEXT NOT NULL,
+            FOREIGN KEY (id) REFERENCES articles (id)
+        );
+        CREATE TABLE IF NOT EXISTS article_authors
+        (
+            id                  TEXT NOT NULL,
+            author              TEXT NOT NULL,
+            author_occupation   TEXT,
+            FOREIGN KEY (id) REFERENCES articles (id)
+        );
+    `);
+}
+
+init_db();
+console.log("Database initialised successfully")
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (_, res) => {
+    res.render("main", {})
+})
+
+app.get("/query", (_req, res) => {
+    res.render("main", {})
+})
+
+app.get("/publish", (_, res) => {
+    res.render("main", {})
+})
+
+app.post("/publish", (_req, res) => {
+
+    res.render("main", {})
 })
 
 app.listen(port, () => {
-    console.log(`Web server ready (${webaddress}:${port})`)
+    console.log(`Web server up (http://localhost:${port})`)
 })
