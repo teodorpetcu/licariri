8704680ad3250cfc2a787cedc2b8e2e36571a6ac
Teodor Petcu <petcuteodor03@gmail.com>
2023-11-01 18:26:20 +0200

    Add a function for indexing the unique words in an article

--- models/query.js
+++ models/query.js
@@ -13,10 +13,13 @@ class QueryDatabase extends Database {
      * Create the database tables if they don't exist already.
      */
     init = () => {
-        this.db.exec(`CREATE TABLE IF NOT EXISTS query
+        // TODO: make more efficient space-wise by associating a word with a
+        // number and having that number map to an article ID inside a different
+        // table
+        this.db.exec(`CREATE TABLE IF NOT EXISTS words
             (
-                word            TEXT NOT NULL,
                 article_id      TEXT NOT NULL,
+                word            TEXT NOT NULL,
                 UNIQUE (word, article_id)
             )`,
             (err) => {
@@ -26,6 +29,22 @@ class QueryDatabase extends Database {
                     console.log(`database '${this.path}' tables: ok`);
                 }
             });
+    }
+
+    /**
+     * Associate every unique word in the `contents` field with the provided ID
+     * @param {string} id - Article ID
+     * @param {string} contents - Contents of the article
+     */
+    indexArticle = async (id, contents) => {
+        let uniqueWords = [... new Set(contents.replace(/[^A-z\-]/g, " ").split(/\s+/))];
+        this.db.serialize(() => {
+            let stmt = this.db.prepare("INSERT INTO words VALUES (?, ?)");
+            for (let word of uniqueWords) {
+                stmt.run([id, word]);
+            }
+            stmt.finalize();
+        });
     }
 }
 
