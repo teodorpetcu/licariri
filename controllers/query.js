3ffdc7428a95e0c5a623da351db62050c08fbb11
Teodor Petcu <petcuteodor03@gmail.com>
2023-12-01 15:29:11 +0200

    Make query parameters cumulative and not additive

--- controllers/query.js
+++ controllers/query.js
@@ -13,8 +13,6 @@ const articleDatabase = new ArticleDatabase(ARTICLE_DATABASE_PATH);
 articleDatabase.init();
 
 const get_queryPage = async (req, res) => {
-    // TODO: maybe send the data to the client and do the searching client-side somehow?
-    // or, you know, write a proper API to handle querying.
     let searchResultsIDs = [];
     let searchResults = [];
 
@@ -30,21 +28,46 @@ const get_queryPage = async (req, res) => {
         tag = any;
         text = any;
     }
+    // NOTE: if the `any` flag is NOT specified, then we return the articles
+    // that match ALL of the provided criteria
+    //
+    // It's probably more efficient to filter-search at the database level, but
+    // for the moment this implementation should *hopefully* not break,
+    // performance-wise
+    //
+    // Overall, the most efficient way to handle this would be to do all this
+    // client-side, since if there would be a frontend interface to properly
+    // handle this it would be ideal if the page wouldn't refresh every time.
 
     if (author) {
-        searchResultsIDs = searchResultsIDs.concat(await articleDatabase.search_articles_by_author(author));
+        let foundArticleIDs = await articleDatabase.search_articles_by_author(author);
+        if (any || !searchResultsIDs.length) {
+            searchResultsIDs = searchResultsIDs.concat(foundArticleIDs);
+        } else {
+            searchResultsIDs = searchResultsIDs.filter((id) => foundArticleIDs.includes(id));
+        }
         if (!tag && !text) {
             message = `Articole scrise de ${author}:`;
         }
     }
     if (tag) {
-        searchResultsIDs = searchResultsIDs.concat(await articleDatabase.search_articles_by_tag(tag));
+        let foundArticleIDs = await articleDatabase.search_articles_by_tag(tag);
+        if (any || !searchResultsIDs.length) {
+            searchResultsIDs = searchResultsIDs.concat(foundArticleIDs);
+        } else {
+            searchResultsIDs = searchResultsIDs.filter((id) => foundArticleIDs.includes(id));
+        }
         if (!author && !text) {
             message = `Articole cu tag-ul ${tag}:`;
         }
     }
     if (text) {
-        searchResultsIDs = searchResultsIDs.concat(await queryDatabase.findArticles(text));
+        let foundArticleIDs = await queryDatabase.findArticles(text);
+        if (any || !searchResultsIDs.length) {
+            searchResultsIDs = searchResultsIDs.concat(foundArticleIDs);
+        } else {
+            searchResultsIDs = searchResultsIDs.filter((id) => foundArticleIDs.includes(id));
+        }
         if (!author && !tag) {
             message = `Articole ce conțin: ${text}`;
         }
