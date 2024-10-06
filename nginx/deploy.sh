bb3d0265df25231ddc23c35c34bcf29d497f4b79
Teodor Petcu <petcuteodor03@gmail.com>
2024-10-06 21:34:49 +0300

    Make nginx deploy script restart nginx

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-cp $(dirname "$0")/licariri.config /etc/nginx/sites-available/licariri.config
+cp $(dirname "$0")/licariri.config /etc/nginx/sites-available/licariri.config \
+    && systemctl restart nginx.service
