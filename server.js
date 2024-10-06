3b465c6907f74c53f0082ed03b150be798d7bf41
Teodor Petcu <petcuteodor03@gmail.com>
2024-10-06 21:30:28 +0300

    Enable expressjs trust proxy

--- server.js
+++ server.js
@@ -50,6 +50,8 @@ const { logger, requestLogger } = require("./logger.js");
 
 const app = express();
 
+app.set("trust proxy", true);
+
 // putting this before other `app.use()` calls makes it not use other middleware
 app.get("/robots.txt", requestLogger, (_, res) => res.sendFile(__dirname + "/robots.txt"));
 // REMEMBER TO ADD `Sitemap` CLAUSE TO robots.txt !!!!
