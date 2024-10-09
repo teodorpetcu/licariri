7b5aab3322224378eb2938306b8722ea494d1cf1
Teodor Petcu <petcuteodor03@gmail.com>
2024-10-10 00:02:38 +0300

    Fix: add hour to backup archive name

--- systemd/licariri-backup.sh
+++ systemd/licariri-backup.sh
@@ -9,5 +9,5 @@ mkdir -p "${BACKUPS_DIRECTORY}"
 
 cd "$(dirname ${APPDATA_PATH})"
 
-tar --force-local --exclude="$(basename ${APPDATA_PATH})/pdfprints" -czf "${BACKUPS_DIRECTORY}/backup-$(date +%Y-%m-%d-%M-%S).tar.gz" "$(basename ${APPDATA_PATH})" \
+tar --force-local --exclude="$(basename ${APPDATA_PATH})/pdfprints" -czf "${BACKUPS_DIRECTORY}/backup-$(date +%Y-%m-%d-%H-%M-%S).tar.gz" "$(basename ${APPDATA_PATH})" \
     && echo "licariri-backup.sh: successfully made a backup of appdata"
