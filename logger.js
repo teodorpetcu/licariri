4b5d80300821a9c86788254d77d7198a6625a21c
Teodor Petcu <petcuteodor03@gmail.com>
2024-10-12 09:49:39 +0300

    Fix: pass error stack to winston

--- logger.js
+++ logger.js
@@ -5,6 +5,7 @@ const { LOG_FILE_PATH } = require("./config.js");
 const logger = winston.createLogger({
     level: "info",
     format: winston.format.combine(
+        winston.format.errors({stack: true}),
         winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         winston.format.printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
     ),
@@ -16,6 +17,7 @@ const logger = winston.createLogger({
 if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({
         format: winston.format.combine(
+            winston.format.errors({stack: true}),
             winston.format.colorize(),
             logger.format,
         )
